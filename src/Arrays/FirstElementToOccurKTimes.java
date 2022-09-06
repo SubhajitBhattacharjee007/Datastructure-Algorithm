6bd776cd56b82d11aac1798a568dd7de50a3d62b
SubhajitHelloWorld <subhajitbhattacharjee007@gmail.com>
2022-09-05 21:47:39 -0400

    FirstElementToOccurKTimes

---
+++ src/Arrays/FirstElementToOccurKTimes.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package Arrays;
+
+/**
+ * @author subhajit
+ * * Given an array of N integers. Find the first element that occurs at least K number of times.
+	 *  N=7, K=2
+	 *  A[]= { 1,7,4,3,4,8,7 } 
+	 *  Output: 4
+	 *  Explanation: Both 7 and 4 occur 2 times
+	 *  but is first that occurs 2 times
+	 *  As at index=4, 4 has occurred atleast 2 times whereas at index=6, 7 has occurred atleast 2 times..
+ *
+ */
+public class FirstElementToOccurKTimes {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
