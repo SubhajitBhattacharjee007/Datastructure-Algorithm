ba007299a9b2acb0ca878a7fba1359bcc39b84d2
SubhajitHelloWorld <subhajitbhattacharjee007@gmail.com>
2023-09-30 11:10:55 -0400

    HAPPY LEARNING!

---
+++ Demo.java
@@ -0,0 +1,32 @@
+package Arrays;
+
+public class Demo {
+
+
+	private String name;
+	private String subject;
+	
+	public Demo() {
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public Demo(String name, String subject) {
+		this.name = name;
+		this.subject = subject;
+	}
+}
